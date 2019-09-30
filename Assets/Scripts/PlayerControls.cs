6b0e4fa2dc8eb5ac836d488f4cc75c7a33c63298
Raphaël Lavoie <rapolysec@hotmail.com>
2019-09-30 16:00:59 -0400

    Personnage controllable de base

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    public float mouseSensitivity = 3.0f;
+
+    float verticalRotation = 0;
+    public float upDownRange = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, rotationLeftRight, 0);
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+        Vector3 speed = new Vector3(sideSpeed,0,forwardSpeed);
+
+        speed = transform.rotation * speed;
+
+        CharacterController controller = GetComponent<CharacterController>();
+        controller.SimpleMove(speed);
+    }
+}
