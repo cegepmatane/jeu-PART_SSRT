2775a4a9b94d82f822db23a43e8d139c911e06d1
Hy-Vong <blobivon@yahoo.fr>
2019-10-04 13:10:43 -0400

    Update prefab tree et scripts

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform treePosition;
+    public Collider treeCollider;
+
+    public float valueMovementSpeed = 10f;
+    private float movementSpeed;
+    private float rotationSpeed = 2f;
+
+    private bool isAttacking = false;
+    public float damage = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = valueMovementSpeed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isAttacking)
+            Attack();
+    }
+
+
+    void FixedUpdate()
+    {
+        //Calcul de la vitesse et de la rotation de l'ennemi
+        float enemySpeed = movementSpeed * Time.deltaTime;
+        float enemyRotation = rotationSpeed * Time.deltaTime;
+        Vector3 direction = treePosition.position - transform.position;
+
+        //Application du mouvement
+        transform.position = Vector3.MoveTowards(transform.position, treePosition.position, enemySpeed);
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+    }
+
+    private void Attack()
+    {
+        treeCollider.gameObject.GetComponent<TreeHealth>().ApplyDamage(damage);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == treeCollider)
+            isAttacking = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider == treeCollider)
+            isAttacking = false;
+    }
+
+}
---
+++ Assets/Scripts/TreeHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHealth : MonoBehaviour
+{
+    public float maxHP = 100f;
+    private float currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyDamage(float a_damage)
+    {
+        currentHP -= a_damage;
+        Debug.Log(currentHP);
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
