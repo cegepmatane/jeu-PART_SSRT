8a366335fe485a3c3da3f0d457a058d2f04de6fd
Raphaël Lavoie <rapolysec@hotmail.com>
2019-10-04 16:00:47 -0400

    Les ennemis spawnés regardent maintenant à la bonne position

--- Assets/Scripts/SpawnerBehavior.cs
+++ Assets/Scripts/SpawnerBehavior.cs
@@ -9,8 +9,8 @@ public class SpawnerBehavior : MonoBehaviour
     public int interval = 1;
     public int delay = 1;
 
-    [SerializeField]
-    private GameObject Enemy, TargetTree;
+    
+    public GameObject Enemy, TargetTree;
     
     
 
@@ -33,18 +33,20 @@ public class SpawnerBehavior : MonoBehaviour
 
     }
 
+    //a_Count représente le nombre d'ennemis à spawner pour ce spawner particulié
+    //a_Interval représente le temps ENTRE chaque création d'un nouvel ennemi
+    //a_Delay représente le délais AVANT de créer des ennemis, donc le temps entre l'arrivée des ennemis et le départ du jeu
     public IEnumerator SpawnEnemy(int a_Count, int a_Interval, int a_Delay)
     {
         // Wait for the delivery delay.
         yield return new WaitForSeconds(a_Delay);
-        Debug.Log("AAA");
+        //Debug.Log("AAA");
         for (; a_Count > 0; a_Count--)
         {
-            bool foundTile = false;
             
             GameObject t_Enemy = Instantiate(Enemy, this.gameObject.transform.position, Quaternion.identity);
             EnemyMovement t_EnemyMovement = t_Enemy.GetComponent<EnemyMovement>();
-            t_EnemyMovement.treePosition = TargetTree.transform;
+            t_EnemyMovement.treePosition = TargetTree.transform.GetChild(0);
             t_EnemyMovement.treeCollider = TargetTree.GetComponent<Collider>();
             yield return new WaitForSeconds(a_Interval);
         }
