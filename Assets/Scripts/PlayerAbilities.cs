29b9b0d745937005e883996c3d2c2b0e5b2b1200
Hy-Vong <blobivon@yahoo.fr>
2019-10-11 14:27:44 -0400

    Mana UI et auto-regeneration

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -1,38 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAbilities : MonoBehaviour
 {
-    private int m_Mana;
-    [SerializeField]
-    private float m_castingCooldown = 3f;
+    private const int  MAX_MANA = 100;
+    private int m_mana;
+
+    private float m_castingCooldown;
+    private float m_manaRegenTimer;
+    private int m_manaRegenSpeed;
+    private bool m_isRegenMana;
 
     [SerializeField]
     private GameObject m_spikeSpell;
     private int m_spikeCost = 10;
 
     private Camera m_camera;
-    [SerializeField]
-    private Canvas m_canvas;
+    private Text m_UiText;
 
     private void Start()
     {
-        m_Mana = 100;
+        m_mana = 100;
+        m_manaRegenTimer = 3f;
+        m_manaRegenSpeed = 2;
+        m_isRegenMana = false;
+
         m_camera = GetComponentInChildren<Camera>();
+        m_UiText = transform.Find("Canvas").transform.Find("Text").gameObject.GetComponent<Text>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //Clic gauche souris
-        if (Input.GetMouseButtonDown(0))
+        //Laisse un délai entre chaque sort
+        m_castingCooldown -= Time.deltaTime;
+        if (m_castingCooldown < 0)
         {
-            if (m_Mana >= m_spikeCost)
+            //Clic gauche de souris
+            if (Input.GetMouseButtonDown(0))
             {
-                Instantiate(m_spikeSpell, transform.position, Quaternion.LookRotation(m_camera.transform.forward));
-                m_Mana -= m_spikeCost;
+                if (m_mana >= m_spikeCost)
+                {
+                    Instantiate(m_spikeSpell, transform.position, Quaternion.LookRotation(m_camera.transform.forward));
+                    m_mana -= m_spikeCost;
+
+                    if (m_isRegenMana)
+                    {
+                        StopCoroutine("ManaRegeneration");
+                        m_isRegenMana = false;
+                    }
+                    m_castingCooldown = 1f;
+                    m_manaRegenTimer = 3f;
+                }
             }
         }
+
+        m_manaRegenTimer -= Time.deltaTime;
+        if (m_manaRegenTimer < 0 && m_mana < MAX_MANA && !m_isRegenMana)
+        {
+            StartCoroutine("ManaRegeneration");
+        }
+
+        updateUI();
     }
 
+    private IEnumerator ManaRegeneration()
+    {
+        m_isRegenMana = true;
+        while (m_mana < MAX_MANA)
+        {
+            m_mana += m_manaRegenSpeed;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        m_isRegenMana = false;
+    }
+
+    private void updateUI()
+    {
+        m_UiText.text = "Mana : " + m_mana;
+    }
 }
