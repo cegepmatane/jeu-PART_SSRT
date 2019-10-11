0a6482362ae487b3ea9e674b732b82e4c58de8db
Raphaël Lavoie <rapolysec@hotmail.com>
2019-10-11 14:17:03 -0400

    Changement de couleur de l'arbre en fonction des dégats reçu

--- Assets/Scripts/TreeHealth.cs
+++ Assets/Scripts/TreeHealth.cs
@@ -22,6 +22,11 @@ public class TreeHealth : MonoBehaviour
     public void ApplyDamage(float a_damage)
     {
         currentHP -= a_damage;
+        Color t_Color = GetComponent<MeshRenderer>().material.color;
+        float t_Greyscale = currentHP/maxHP;
+        t_Color.a = t_Greyscale;
+        Debug.Log("Greyscale = " + t_Greyscale);
+        GetComponent<MeshRenderer>().material.color = new Color(t_Greyscale,t_Greyscale,t_Greyscale);
         Debug.Log(currentHP);
         if (currentHP <= 0)
         {
