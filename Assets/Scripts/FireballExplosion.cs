707e75de50dee9c363486a48bbc21258357c8a58
Hy-Vong <blobivon@yahoo.fr>
2019-11-08 15:35:41 -0500

    Import des Unity Particle Effects et ajout d'une explosion pour la fireball qui fait des dégâts autour

--- Assets/Scripts/FireballExplosion.cs
+++ Assets/Scripts/FireballExplosion.cs
@@ -7,15 +7,42 @@ public class FireballExplosion : MonoBehaviour
     private AudioSource m_AudioSource;
     public AudioClip m_AudioClip;
 
+    [SerializeField] private int m_ExplosionDamage;
+    [SerializeField] private float m_ExplosionRadius;
+
     // Start is called before the first frame update
     private void Start()
     {
         m_AudioSource = GetComponent<AudioSource>();
 
+        PlayExplosionSound();
+        ApplyExplodingDamage();
+        //TODO Orienter les particules pour qu'elles se deplacent sur la normal du point touché et pas à travers
+
+        Destroy(gameObject, 2f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, m_ExplosionRadius);
+    }
+
+    private void PlayExplosionSound()
+    {
         m_AudioSource.clip = m_AudioClip;
         m_AudioSource.spatialBlend = 1f;
         m_AudioSource.Play();
+    }
 
-        Destroy(gameObject, 2f);
+    private void ApplyExplodingDamage()
+    {
+        Collider[] t_TouchedObjectsColliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius);
+        foreach(Collider col in t_TouchedObjectsColliders)
+        {
+            if (col.gameObject.GetComponent<EnemyHealth>() != null)
+                col.gameObject.GetComponent<EnemyHealth>().TakeDamage(m_ExplosionDamage);
+        }
+
     }
 }
