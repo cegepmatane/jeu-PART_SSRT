0f85d7b5a3e592406bd56853087413b6e2e5ed87
Jonathan Rousseau <jonathan.rousseau33@gmail.com>
2019-11-08 16:00:45 -0500

    Spawner between bounds

---
+++ Assets/Scripts/SpawnerBounds.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBounds : MonoBehaviour
+{
+    public GameObject Prefab;
+
+    public Transform Bound1, Bound2;
+    public float RaycastLenght = 100;
+    public int MaxTries = 10;
+
+    public LayerMask HitLayers;
+
+    /*private void Start()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            Spawn();
+        }
+    }*/
+
+    public bool Spawn()
+    {
+        RaycastHit t_Hit;
+        bool t_DidHit = false; ;
+
+        for (int i = 0; i < MaxTries; i++)
+        {
+            float t_PosX = Mathf.Lerp(Bound1.position.x, Bound2.position.x, Random.value);
+            float t_PosZ = Mathf.Lerp(Bound1.position.z, Bound2.position.z, Random.value);
+
+            
+            t_DidHit = Physics.Raycast(new Vector3(t_PosX, transform.position.y, t_PosZ), Vector3.down, out t_Hit, RaycastLenght, HitLayers);
+
+            if (t_DidHit)
+            {
+                Instantiate(Prefab, t_Hit.point, Prefab.transform.rotation);
+                break;
+            }
+        }
+
+        if (!t_DidHit)
+            Debug.Log(gameObject.name + " tried to Spawn, but did not find a valid location");
+
+
+        return t_DidHit;
+    }
+}
